093dae1c829b5543f0bbf0e416672624e2efef4b
ruisharp <liuruizx@126.com>
2016-04-22 17:24:53 +0800

    first

---
+++ GitTest/src/org/test/Main.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package org.test;
+
+/**   
+ * @ClassName: Main  
+ * @Package org.test 
+ * @Description: TODO
+ * @date 2016年4月22日 下午4:08:29 
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("ddhello");
+	}
+
+}
